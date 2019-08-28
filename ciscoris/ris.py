0cc143e09c10c948a625e94e1f6e3c46374c93a3
Jeff Levensailor <jeff@levensailor.com>
2019-08-27 23:07:36 -0700

    updated to use zeep

--- ciscoris/ris.py
+++ ciscoris/ris.py
@@ -1,22 +1,28 @@
 """
 Class to interface with cisco ucm ris api.
 Author: Jeff Levensailor
-Version: 1.0.0
+Version: 0.0.5
 Dependencies:
- - suds-jurko: https://bitbucket.org/jurko/suds
+ - zeep: https://python-zeep.readthedocs.io/en/master/
 
 Links:
  - https://developer.cisco.com/docs/sxml/#risport70-api-reference
 """
 
-import ssl
-import urllib
+import sys
+import pathlib
+import os
 
-from suds.transport.https import HttpAuthenticated
-from suds.client import Client
-
-from suds.xsd.doctor import Import
-from suds.xsd.doctor import ImportDoctor
+from requests import Session 
+from requests.auth import HTTPBasicAuth 
+import re
+import urllib3 
+from zeep import Client, Settings, Plugin 
+from zeep.transports import Transport 
+from zeep.cache import SqliteCache 
+from zeep.plugins import HistoryPlugin 
+from zeep.exceptions import Fault 
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
 class ris(object):
@@ -36,33 +42,25 @@ class ris(object):
         >>> from ris import RIS
         >>> ucm = RIS('ris_user', 'ris_pass', '192.168.200.10', '11.5')
         """
+        wsdl = 'https://'+cucm+':8443/realtimeservice2/services/RISService70?wsdl'
+        session = Session() 
+        session.verify = False 
+        session.auth = HTTPBasicAuth(username, password) 
+        settings = Settings(strict=False, xml_huge_tree=True) 
+        transport = Transport(session=session, timeout=10, cache=SqliteCache()) 
+        ris_client = Client(wsdl, settings=settings, transport=transport) 
+
+
+
+        self.wsdl = wsdl
         self.username = username
         self.password = password
-        self.wsdl = 'https://'+cucm+':8443/realtimeservice2/services/RISService70?wsdl'
         self.cucm = cucm
         self.cucm_version = cucm_version
-
-
-        tns = 'http://schemas.cisco.com/ast/soap/'
-        imp = Import('http://schemas.xmlsoap.org/soap/encoding/', 'http://schemas.xmlsoap.org/soap/encoding/')
-        imp.filter.add(tns)
-
-        t = HttpAuthenticated(username=self.username, password=self.password)
-        t.handler = urllib.request.HTTPBasicAuthHandler(t.pm)
-        
-        ssl_def_context = ssl.create_default_context()
-        ssl_def_context.check_hostname = False
-        ssl_def_context.verify_mode = ssl.CERT_NONE
-
-        t1 = urllib.request.HTTPSHandler(context=ssl_def_context)
-        t.urlopener = urllib.request.build_opener(t.handler, t1)
-
-        self.client = Client(self.wsdl, location='https://{0}:8443/realtimeservice2/services/RISService70'.format(cucm), faults=False,
-                             plugins=[ImportDoctor(imp)],
-                             transport=t)
+        self.UUID_PATTERN = re.compile(r'^[\da-f]{8}-([\da-f]{4}-){3}[\da-f]{12}$', re.IGNORECASE)
+        self.client = ris_client.create_service("{http://schemas.cisco.com/ast/soap}RisBinding", f"https://{cucm}:8443/realtimeservice2/services/RISService70")
 
     def get_devices(self, **args):
-
         def parse(registered):
             if registered['CmNodes'] is not None:
                 for node in registered.CmNodes:
@@ -70,22 +68,12 @@ class ris(object):
                         for dev in item[1]:
                             if dev['Status'] == "Registered":
                                 return dev
-            else: return
-
-        resp = self.client.service.selectCmDeviceExt("", args)
-        result = {
-            'success': False,
-            'response': '',
-            'error': '',
-        }
-        if resp[0] == 200:
-            result['success'] = True
-            result['response'] = parse(resp[1]['SelectCmDeviceResult'])
-            return result
-        else:
-            result['response'] = 'Unknown error'
-            result['error'] = resp[1].faultstring
-            return result
+            else: 
+                return
+        try:
+            return self.client.selectCmDeviceExt("", args)['SelectCmDeviceResult']
+        except Fault as e:
+            return e
 
     def checkRegistration(self, phones, subs):
         CmSelectionCriteria = {
@@ -107,5 +95,4 @@ class ris(object):
             CmSelectionCriteria['NodeName'] = sub
             CmSelectionCriteria['SelectItems']['item']['Item'] = ",".join(phones)
             reg = self.get_devices(**CmSelectionCriteria)
-            if reg['success']:
-                return reg['response']
+            return reg
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="ciscoris",
-    version="0.0.4",
+    version="0.0.5",
     author="Jeff Levensailor",
     author_email="jeff@levensailor.com",
     description="Cisco CUCM RIS Library. Simple to use.",
@@ -17,8 +17,10 @@ setup(
     packages=['ciscoris'],
     include_package_data=True,
     install_requires = [
-    'suds-jurko==0.6',
-    'urllib3==1.23'
+    'zeep==3.4.0',
+    'urllib3==1.23',
+    'requests==2.22.0',
+    'six==1.12.0'
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
